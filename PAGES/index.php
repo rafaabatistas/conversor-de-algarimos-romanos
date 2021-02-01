8ca6a9ec14ed7104c045bc1ba5f69acc16692af4
Rafael Batista <65174147+RafaaBatistasS@users.noreply.github.com>
2021-02-01 13:39:01 -0300

    New user restriction on Index.html

--- PAGES/index.php
+++ PAGES/index.php
@@ -13,32 +13,31 @@
 </head>
 <body>
     <div class="box-lembrete">
-        <?php           
-            if(empty($numeroRomano)) : ?>
-                <h1>Digite os números Romanos aqui</h1>
-                <form class="form-principal" method="post">
-                    <div class="box-text">
-                        <input class="text" type="text" name="algarismo" placeholder="Ex: XV" max="7" maxlength="7" required>
-                    </div>
-                    <button class="box-button" formaction="index.php">Ok!</button>
-                </form>
-                <?php elseif(is_numeric($numeroRomano) == 1) : ?>
-                    <h1>Opss!</h1>
-                        <form class="form-principal" method="get">
-                            <div class="box-text">
-                            <p><?= $numeroRomano . ' não é um número romano. ' ?></p>
-                            </div>
-                            <button class="box-button"> </a> Voltar</button>
-                        </form>
-                    <?php else : ?>
-                        <h1>O resultado é!?</h1>
-                        <form class="form-principal" method="get">
-                            <div class="box-text">
-                            <p><?= $numeroRomano . ' = ' . conversorInt($numeroRomano) ?></p>
-                            </div>
-                            <button class="box-button"> </a> Voltar</button>
-                        </form>
-            <?php endif; ?>
+        <?php if(empty($numeroRomano)) : ?>
+            <h1>Digite os números Romanos aqui</h1>
+            <form class="form-principal" method="post">
+                <div class="box-text">
+                    <input class="text" type="text" name="algarismo" placeholder="Ex: XV" max="7" maxlength="7" required>
+                </div>
+                <button class="box-button" formaction="index.php">Ok!</button>
+            </form>
+            <?php elseif(is_numeric($numeroRomano) == 1 || conversorInt($numeroRomano) === 0) : ?>
+                <h1>Opss!</h1>
+                    <form class="form-principal" method="get">
+                        <div class="box-text">
+                        <p><?= $numeroRomano . ' não é um número romano. ' ?></p>
+                        </div>
+                        <button class="box-button"> </a> Voltar</button>
+                    </form>
+                <?php else : ?>
+                    <h1>O resultado é!?</h1>
+                    <form class="form-principal" method="get">
+                        <div class="box-text">
+                        <p><?= $numeroRomano . ' = ' . conversorInt($numeroRomano) ?></p>
+                        </div>
+                        <button class="box-button"> </a> Voltar</button>
+                    </form>
+        <?php endif; ?>
     </div>
     <div class="box-shadow"></div>
 </body>
